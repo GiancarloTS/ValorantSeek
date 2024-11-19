82fff66f947e30d60f24dffae92dcda2839ec895
GiancarloTS <164694231+GiancarloTS@users.noreply.github.com>
2024-11-19 20:21:28 -0300

    Delete ValorantSeek directory

--- ValorantSeek/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ValorantSeek',
-  webDir: 'www'
-};
-
-export default config;
