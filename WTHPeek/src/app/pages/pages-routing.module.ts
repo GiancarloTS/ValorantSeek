fd725bdf08aba0d814e9986e6b8587965191a42e
GiancarloTS <164694231+GiancarloTS@users.noreply.github.com>
2024-11-21 00:42:03 -0300

    mejorando

--- WTHPeek/src/app/pages/pages-routing.module.ts
+++ WTHPeek/src/app/pages/pages-routing.module.ts
@@ -9,6 +9,11 @@ const routes: Routes = [
   },
   {path:'login',loadChildren: () => import('./login/login.module').then( m => m.LoginModule)},
   {path:'vehiculo',loadChildren: () => import('./vehicle-search/vehicle-search.module').then( m => m.VehicleSearchPageModule)},
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  },
+
 ]
 @NgModule({
   imports: [RouterModule.forChild(routes)],
