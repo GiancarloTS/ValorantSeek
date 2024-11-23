122aafabfb50be37e48e49a5481c752ae1204a74
Matias <enduque.77@gmail.com>
2024-11-23 04:10:20 -0300

    mejoras de style

--- WTHPeek/src/app/pages/inicio/inicio.page.ts
+++ WTHPeek/src/app/pages/inicio/inicio.page.ts
@@ -30,4 +30,21 @@ export class InicioPage implements OnInit {
       }
     );
   }
+  // Conversión de BBCode a HTML
+  formatBBCode(content: string): string {
+    if (!content) return '';
+
+    const steamImageBaseURL = 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/clans';
+
+    return content
+      .replace(/\[img\]\{STEAM_CLAN_IMAGE\}(.*?)\[\/img\]/g, `<img src="${steamImageBaseURL}$1" alt="Image">`)
+      .replace(/\[h1\](.*?)\[\/h1\]/g, '<h1>$1</h1>')
+      .replace(/\[h2\](.*?)\[\/h2\]/g, '<h2>$1</h2>')
+      .replace(/\[h3\](.*?)\[\/h3\]/g, '<h3>$1</h3>')
+      .replace(/\[b\](.*?)\[\/b\]/g, '<strong>$1</strong>')
+      .replace(/\[url=(.*?)\](.*?)\[\/url\]/g, '<a href="$1" target="_blank">$2</a>')
+      .replace(/\[list\](.*?)\[\/list\]/gs, '<ul>$1</ul>')
+      .replace(/\[\*\](.*?)$/gm, '<li>$1</li>')
+      .replace(/\[\/?\w+\]/g, ''); // Eliminar etiquetas no soportadas
+  }
 }
